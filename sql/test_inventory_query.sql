cad7cd3a0c34ef07dbbe1065d6321dc2dbf5a21b
I-am-Future <3038242641@qq.com>
2023-04-16 18:26:42 +0800

    Added data files

--- sql/test_inventory_query.sql
+++ sql/test_inventory_query.sql
@@ -1,11 +1,14 @@
 use riimages;
+
 -- INSERT food(`RESTAURANT_ID`,`FOOD_TYPE`,`FOOD_NAME`,`PRICE`,`INVENTORY`) values(
 --     114514, 'xue', 'yinbinjiu', 114514, 18);
 -- INSERT food(`RESTAURANT_ID`,`FOOD_TYPE`,`FOOD_NAME`,`PRICE`,`INVENTORY`) values(
 --     114514, 'xue', 'yinbinjiu2', 114, 51);
+
 -- SELECT FOOD_TYPE, FOOD_NAME, PRICE, INVENTORY
 -- FROM food
 -- where (RESTAURANT_ID = 1);
+
 Update food
 SET INVENTORY = 999
 WHERE RESTAURANT_ID = 114514
--- sql/test_restaurant_insert.sql
+++ sql/test_restaurant_insert.sql
@@ -1,4 +1,6 @@
 use `riimages`;
 
-insert restaurant(`RESTAURANT_NAME`,`RESTAURANT_ADDRESS`,
-	`SALES`,`LOGIN_NAME`,`LOGIN_PASSWORD`) values ('Happy Meal Time', 'xuehuo', 1.1, 'aaa', '111');
+-- insert restaurant(`RESTAURANT_NAME`,`RESTAURANT_ADDRESS`,
+-- 	`SALES`,`LOGIN_NAME`,`LOGIN_PASSWORD`) values ('Happy Meal Time', 'xuehuo', 1.1, 'aaa', '111');
+
+delete from restaurant where RESTAURANT_ID = 3;
