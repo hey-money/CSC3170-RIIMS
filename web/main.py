835cbf8b14b677b6cbd6b543e1e98ef404ee2674
I-am-Future <3038242641@qq.com>
2023-04-17 21:51:33 +0800

    Added features for background img

--- web/main.py
+++ web/main.py
@@ -30,6 +30,23 @@ from utils import control as control_util
 
 st.set_page_config(page_title=config.ProjName, page_icon='assets/online-library.png')
 
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('assets/bkg2_33.jpg')    
+    
 
 if "function" not in st.session_state: 
     # Log in for the first time
