24927d2990fe0c03270791ab968bc492b2783129
I-am-Future <3038242641@qq.com>
2023-04-18 10:23:08 +0800

    Updated analysis template

--- web/pagelib/backend/analysis_page.py
+++ web/pagelib/backend/analysis_page.py
@@ -16,12 +16,36 @@ def foo():
 def turnover_analysis_page():
     ##
     st.title("Turnover Analysis")
-    foo()
+
+    viewmode = st.radio("View Mode", ('Day', 'Week', 'Month'))
+    st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+    # fetch data
+
+    # show the data
+    chart_data = pd.DataFrame(
+        np.random.rand(50, 3) * 10,
+        columns = ["a", "b", "c"])
+    st.bar_chart(chart_data)
+
 
 def dish_analysis_page():
     ##
     st.title("Dish Analysis")
-    foo()
+    
+    options = st.multiselect(
+        'Please enter the dishes you want to inspect:',
+        ['Green', 'Yellow', 'Red', 'Blue'],
+        ['Yellow', 'Red'])
+    # fetch data
+
+    # show the data
+    chart_data = pd.DataFrame(
+        np.random.randn(20, len(options)),
+        columns=options)
+
+    st.line_chart(chart_data)
+
+    st.text(f'You selected: {options}')
 
 def dish_data_mining_page():
     ##
