a9d0066a90c98787b071e1bfbe75882ccbbe9bd5
Zekai Chen <120090539@link.cuhk.edu.cn>
2023-04-18 20:52:42 +0800

    added analysis

--- analysis/sql_tools.py
+++ analysis/sql_tools.py
@@ -13,7 +13,7 @@ def create_session_cursor():
             host = '127.0.0.1',
             port = 3306,
             user = 'root',
-            password = '123456',   # change to your password!!!
+            password = 'czk20011201',   # change to your password!!!
             database = 'riimages',
             buffered = True
         ) 
