6a10ddd7d49eb94d6289072a9bac7bee7f2319c6
I-am-Future <3038242641@qq.com>
2023-04-19 23:38:14 +0800

    Added dish sales prediction

--- web/pagelib/backend/data_mining_page.py
+++ web/pagelib/backend/data_mining_page.py
@@ -8,11 +8,25 @@ import pandas as pd
 import numpy as np
 
 from utils import sql
+from sklearn.linear_model import LinearRegression
 
 from pagelib.backend._analysis_page import(
     _fetch_foodorder_data,
     _process_dish_analysis
 )
+
+
+def _LR_predict_selling(previous: pd.DataFrame, forecast_days=5) -> pd.DataFrame:
+    X = np.arange(len(previous)).reshape(-1, 1)
+    Y = previous.copy().to_numpy()
+
+    reg = LinearRegression().fit(X,Y)
+    Predict_X = np.arange(len(previous) + forecast_days).reshape(-1, 1)
+
+    Predict_Y = reg.predict(Predict_X)
+
+    return Predict_X, Predict_Y
+
 
 def dish_data_mining_page() -> None:
     ##
@@ -32,9 +46,15 @@ def dish_data_mining_page() -> None:
     dish = st.selectbox('Select a dish to predict:', result.columns)
 
     alg_select = st.radio('Prediction Methods:', 
-             ['Linear Regression', 'ARMA (Will Support Later)'])
+             ['Linear Regression', 'ARMA (To Be Supported Later)'])
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
-    st.line_chart(result[dish])
+    
+    PredictionX, PredictionY = _LR_predict_selling(result[dish])
 
-    # print(alg_select)
+    ans = pd.DataFrame({"Predicted": PredictionY}, index=pd.date_range(result.index[0], periods=len(result)+5))
+    ans[dish] = np.concatenate([result[dish].to_numpy(), np.array([np.nan]*5)])
+
+    st.line_chart(ans)
+
+
