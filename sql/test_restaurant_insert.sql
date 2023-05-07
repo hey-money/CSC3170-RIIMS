32f06941f833cfdabb31dd757a31343ae0c8d952
I-am-Future <3038242641@qq.com>
2023-05-07 11:55:30 +0800

    Rearrange the files

--- sql/test_restaurant_insert.sql
+++
@@ -1,6 +0,0 @@
-use `riimages`;
-
--- insert restaurant(`RESTAURANT_NAME`,`RESTAURANT_ADDRESS`,
--- 	`SALES`,`LOGIN_NAME`,`LOGIN_PASSWORD`) values ('Happy Meal Time', 'xuehuo', 1.1, 'aaa', '111');
-
-delete from restaurant where RESTAURANT_ID = 3;
